a31569fc33d6846457ac9a7057bed5c48ecd7d63
Bhushan <dateybhushan@yahoo.com>
2023-01-13 15:59:40 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,17 @@
+public class Practice 
+{
+
+	public static void main(String[] args) 
+  {
+	
+		
+		System.out.println("Hello World");
+
+		
+		System.out.println("Hi World");
+		
+		System.out.println("Bye World");
+		
+	}
+
+}
